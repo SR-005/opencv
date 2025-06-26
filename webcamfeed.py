5901a2df8dca3241b9144d7a238ee9bd0a4d8b97
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-06-26 13:21:02 +0530

    polished brightness

--- webcamfeed.py
+++ webcamfeed.py
@@ -3,6 +3,7 @@ import cv2
 feed = cv2.VideoCapture(0, cv2.CAP_DSHOW) #if you have only one webcam- 0 reads feed fom that one
 feed.set(3,640) #setting up its width - id 3 refers to width
 feed.set(4,480) #setting up its height - id 4 refers to height
+feed.set(10,100) #id 10 is for BRIGHTNESS
 
 while True:
     success,img=feed.read()
