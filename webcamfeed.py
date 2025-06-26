89643a98ee005f8e5d77ce0139a12b573719fe5e
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-06-26 13:19:26 +0530

    first feed setup finished

--- webcamfeed.py
+++ webcamfeed.py
@@ -1,11 +1,11 @@
 import cv2
 
-feed=cv2.VideoCapture(0) #if you have only one webcam- 0 reads feed fom that one
+feed = cv2.VideoCapture(0, cv2.CAP_DSHOW) #if you have only one webcam- 0 reads feed fom that one
 feed.set(3,640) #setting up its width - id 3 refers to width
 feed.set(4,480) #setting up its height - id 4 refers to height
 
 while True:
     success,img=feed.read()
     cv2.imshow("Video",img)
-    if cv2.waitKey(1):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
