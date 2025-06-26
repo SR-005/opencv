0994360546cdfff7f7016c88dd142fe568d202c5
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-06-26 20:34:32 +0530

    started formality code for tracking with mideapipe

--- handtracking.py
+++ handtracking.py
@@ -1,4 +1,17 @@
 import cv2
 import mediapipe as mp
 
-print("hello")
+feed = cv2.VideoCapture(0, cv2.CAP_DSHOW) #if you have only one webcam- 0 reads feed fom that one
+feed.set(3,640) #setting up its width - id 3 refers to width
+feed.set(4,480) #setting up its height - id 4 refers to height
+feed.set(10,100) #id 10 is for BRIGHTNESS 
+
+#DEFAULT FORMALITY!!!!
+mphands=mp.solutions.hands
+hands=mphands.Hands()
+
+while True:
+    success,img=feed.read()
+    cv2.imshow("Video",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
